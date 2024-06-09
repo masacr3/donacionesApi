d2000ff1ea4e67fac9b5a349f575ec1bc12bf18c
masacr3 <leorhcp2030@gmail.com>
2024-06-09 03:41:34 -0300

    subo los jar

--- src/main/java/com/masacr3/donacionesApi/WebConfig.java
+++ src/main/java/com/masacr3/donacionesApi/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","http://192.168.0.69:5173","https://masacr3.github.io/donacionesTetu") // Agrega todos los dominios necesarios
+                .allowedOrigins("https://masacr3.github.io/donacionesTetu") // Agrega todos los dominios necesarios
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
